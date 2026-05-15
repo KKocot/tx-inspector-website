490e22aa10263f3449360236314b873cdef14f07
Krzysztof Kocot <k.kocot0@gmail.com>
2026-05-15 10:09:02 +0200

    chore: update gitignore, config, and design tokens

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
  * Import these instead of hardcoding URLs in components.
  */
 
-export const APP_URL = "https://tx-inspector.hive.io/app";
 export const GITLAB_URL = "https://gitlab.syncad.com/hive/tx-inspector";
 export const BLOCK_EXPLORER_URL = "https://explore.openhive.network";
 export const HIVE_URL = "https://hive.io";
+export const HIVE_BRIDGE_URL = "https://auth.openhive.network/";
