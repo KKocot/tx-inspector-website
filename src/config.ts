1db9a3b406ae224ae935b6c1cb4600e18ea79ced
Krzysztof Kocot <k.kocot0@gmail.com>
2026-05-28 12:12:00 +0200

    feat: add Hive Tools link to footer

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,4 @@ export const BLOCK_EXPLORER_URL = "https://explore.openhive.network";
 export const HIVE_URL = "https://hive.io";
 export const HIVE_BRIDGE_URL = "https://auth.openhive.network/";
 export const BLOCKCHAIN_WARES_URL = "https://blockchainwares.com.pl/";
+export const HIVE_TOOLS_URL = "https://tools.openhive.network";
